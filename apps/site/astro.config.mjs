293f1394acd6b3b8f6eb1de58ef86c850fb8ad96
Nick Taylor <nick@iamdeveloper.com>
2023-03-04 18:55:08 -0500

    chore: added sitemap.xml

--- apps/site/astro.config.mjs
+++ apps/site/astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 const isProduction = process.env.NODE_ENV === 'production';
 
 // https://astro.build/config
 export default defineConfig({
   outDir: '../../dist/apps/site',
-  integrations: [mdx()],
+  integrations: [mdx(), sitemap()],
   // TODO: Change this to https://iamdeveloper.com when before the site goes live
   site: isProduction ? 'https://iamdeveloper-dot-com-beta.netlify.app' : 'http://localhost:3000',
 });
