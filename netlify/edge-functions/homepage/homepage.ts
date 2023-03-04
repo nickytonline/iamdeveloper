a827476b9adb16657a5f69397a91c7d6f32d69d2
Nick Taylor <nick@iamdeveloper.com>
2023-03-03 23:17:04 -0500

    chore: now homepage loads upcoming live stream guest

---
+++ netlify/edge-functions/homepage/homepage.ts
@@ -0,0 +1,53 @@
+import { Context } from 'https://edge.netlify.com';
+import { getLatestGuestMarkup } from '../utils/markupUtils.ts';
+import { getLatestGuest } from '../utils/streamUtils.ts';
+
+const AIRTABLE_API_KEY = Deno.env.get('AIRTABLE_API_KEY');
+const AIRTABLE_STREAM_GUEST_BASE_ID = Deno.env.get(
+  'AIRTABLE_STREAM_GUEST_BASE_ID'
+);
+
+if (!AIRTABLE_API_KEY || !AIRTABLE_STREAM_GUEST_BASE_ID) {
+  throw new Error('Missing required environment variables');
+}
+
+export default async (request: Request, context: Context) => {
+  // Locale is the first language in the Accept-Language header or 'en-US' if not present
+  const locale =
+    request.headers.get('Accept-Language')?.split(',')[0] ||
+    request.headers.get('Accept-Language') ||
+    'en-US';
+  const { timezone = '' } = context.geo;
+
+  const latestGuest = await getLatestGuest({
+    apiKey: AIRTABLE_API_KEY,
+    baseId: AIRTABLE_STREAM_GUEST_BASE_ID,
+  });
+  const scheduleMarkup = getLatestGuestMarkup({
+    guest: latestGuest,
+    locale,
+    timezone,
+  });
+
+  const response = await context.next();
+
+  // Cache the response for 24 hours
+  response.headers.set(
+    'Cache-Control',
+    'public, max-age=86400, s-maxage=86400'
+  );
+  response.headers.set('x-timezone', timezone);
+  response.headers.set('Vary', 'Accept-Language, x-timezone');
+
+  const text = await response.text();
+
+  return new Response(
+    text.replace('<!-- replace-with-stream-guest -->', scheduleMarkup),
+    response
+  );
+};
+
+export const config = {
+  cache: 'manual',
+  path: '/',
+};
---
+++ netlify/edge-functions/utils/StreamGuestInfo.ts
@@ -0,0 +1,13 @@
+export interface StreamGuestInfo {
+  date: string;
+  streamTitle: string;
+  streamDescription: string;
+  name: string;
+  title?: string;
+  twitter?: string;
+  youtube?: string;
+  twitch?: string;
+  github?: string;
+  website?: string;
+  polywork?: string;
+}
