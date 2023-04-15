a7cf9f93b7e1e62e2210b042f033a5bb4c4aff8c
Nick Taylor <nick@iamdeveloper.com>
2023-04-15 08:12:20 -0400

    chore: configured astro for markdoc

--- apps/site/astro.config.mjs
+++ apps/site/astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
+import markdoc from "@astrojs/markdoc";
 const isProduction = process.env.NODE_ENV === 'production';
+
+// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
   outDir: '../../dist/apps/site',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), markdoc()],
   // TODO: Change this to https://iamdeveloper.com when before the site goes live
-  site: isProduction ? 'https://iamdeveloper-dot-com-beta.netlify.app' : 'http://localhost:3000',
+  site: isProduction ? 'https://iamdeveloper-dot-com-beta.netlify.app' : 'http://localhost:3000'
 });
